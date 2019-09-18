7d5b18e72441e37662d93c30f0270f46ee28d3e7
vbpawar <vikaspawar3110@gmail.com>
2019-09-18 19:36:25 +0530

    add changes in coe

--- leftsidebar.php
+++ leftsidebar.php
@@ -29,29 +29,6 @@
                 <li> <a class="has-arrow  " href="customers.php" aria-expanded="false"><i
                             class="fa fa-tachometer"></i><span class="hide-menu">Order Management</span></a>
 
-<<<<<<< HEAD
-                    </ul>
-                </li> -->
-                <li><a class="has-arrow  " href="customerappointment.php" aria-expanded="false"><i class="fa fa-gift"></i><span class="hide-menu">Customer Appointment</span></a></li>
-                <li ><a class="has-arrow  " href="masterproducts.php" aria-expanded="false"><i class="fa fa-gift"></i><span class="hide-menu">Master Products</span></a></li>
-                <li ><a class="has-arrow  " href="products.php" aria-expanded="false"><i class="fa fa-gift"></i><span class="hide-menu">Products</span></a></li>
-                <li ><a class="has-arrow  " href="customerstyles.php" aria-expanded="false"><i class="fa fa-gift"></i><span class="hide-menu">Styles</span></a></li>
-                <li><a class="has-arrow  " href="customersubstyles.php" aria-expanded="false"><i class="fa fa-gift"></i><span class="hide-menu">Substyles</span></a></li>
-                <li ><a class="has-arrow  " href="managecategory.php" aria-expanded="false"><i class="fa fa-gift"></i><span class="hide-menu">Manage Product Category</span></a></li>
-                  <li ><a class="has-arrow  " href="measurement.php" aria-expanded="false"><i class="fa fa-gift"></i><span class="hide-menu">Measurements</span></a></li>
-                <li><a class="has-arrow  " href="fabric.php" aria-expanded="false"><i class="fa fa-plus"></i><span class="hide-menu">Fabric</span></a></li>
-                <li ><a class="has-arrow  " href="stitchstyle.php" aria-expanded="false"><i class="fa fa-gift"></i><span class="hide-menu">Stitch-Style</span></a></li>
-                <li><a class="has-arrow  " href="stitchstyledetail.php" aria-expanded="false"><i class="fa fa-gift"></i><span class="hide-menu">Stitch-Style-Detail-Item</span></a></li>
-                <li> <a class="has-arrow  " href="customers.php" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">Create Order</span></a></li>
-                <!-- <li class="nav-label">Apps</li>
-                <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-envelope"></i><span class="hide-menu">Appointment</span></a>
-                    <ul aria-expanded="false" class="collapse">
-                        <li><a href="email-compose.html">Compose</a></li>
-                        <li><a href="email-read.html">Read</a></li>
-                        <li><a href="email-inbox.html">Inbox</a></li>
-                    </ul>
-=======
->>>>>>> 65b0eb3eebfdf7dc7611676165c0fe41c1832ee6
                 </li>
 
             </ul>
