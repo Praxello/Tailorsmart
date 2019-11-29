fdd297c9d150b11ecc1f155cc84195240d08367f
vbpawar <vikaspawar3110@gmail.com>
2019-11-29 14:25:15 +0530

    add changes

--- createNewOrder.php
+++ createNewOrder.php
@@ -11,6 +11,9 @@
         <li class="nav-item">
             <a class="nav-link" data-toggle="tab" href="#OrderStatus"><i class="fa fa fa-rupee"></i>Order Status</a>
         </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#appointments"><i class="fa fa fa-rupee"></i>Appointments</a>
+        </li>
     </ul>
     <div class="tab-content">
         <div id="productHome" class="tab-pane active"><br>
@@ -20,8 +23,7 @@
                         <div class="row">
 
                             <div class="col-sm-12">
-                            <a href="#" class="text-primary" data-toggle="collapse" data-target="#demo" style="font-weight:bolder;"> Order Details<i
-                                        class="fa fa-close float-right"></i></a>
+                                <a href="#" class="text-primary" data-toggle="collapse" data-target="#demo" style="font-weight:bolder;"> Order Details<i class="fa fa-close float-right"></i></a>
                             </div>
                         </div>
                         <div id="demo" class="collapse">
@@ -61,8 +63,7 @@
                         <div class="row">
 
                             <div class="col-sm-12">
-                                <a href="#" class="text-primary" data-toggle="collapse" data-target="#demo1" style="font-weight:bolder;" > Customer Details<i
-                                        class="fa fa-close float-right"></i></a>
+                                <a href="#" class="text-primary" data-toggle="collapse" data-target="#demo1" style="font-weight:bolder;"> Customer Details<i class="fa fa-close float-right"></i></a>
                             </div>
                         </div>
 
@@ -119,23 +120,20 @@
                             <div class="col-sm-3">
                                 <div class="form-group">
 
-                                    <select class="form-control form-control-sm" id="products" style="width:100%"
-                                        onchange="getPriceOfProduct(this.value)">
+                                    <select class="form-control form-control-sm" id="products" style="width:100%" onchange="getPriceOfProduct(this.value)">
                                     </select>
                                 </div>
                             </div>
                             <div class="col-sm-3">
                                 <div class="form-group">
-                                    <input type="text" class="form-control form-control-sm" name="OrderItemPrice"
-                                        id="OrderItemPrice">
+                                    <input type="text" class="form-control form-control-sm" name="OrderItemPrice" id="OrderItemPrice">
                                 </div>
                             </div>
                             <div class="col-sm-3">
                                 <div class="btn-group" role="group" aria-label="add example">
                                     <button type="button" class="add-row btn btn-success"><i class="fa  fa-plus"></i>Add
                                         Product</button>
-                                    <button type="button" class="btn btn-primary" id="loadfirstpage"><i
-                                            class="fa  fa-arrow-left"></i>Go Back</button></div>
+                                    <button type="button" class="btn btn-primary" id="loadfirstpage"><i class="fa  fa-arrow-left"></i>Go Back</button></div>
                             </div>
                             <div class="col-sm-3">
 
@@ -170,11 +168,10 @@
         <div id="paymentlink" class="tab-pane"><br>
             <div class="card">
                 <div class="form-group ">
-                    <button type="button" class="btn btn-secondary float-right" data-toggle="modal"
-                        data-target="#paymentLinkModal">Create Payment Link</button>
+                    <button type="button" class="btn btn-secondary float-right" data-toggle="modal" data-target="#paymentLinkModal">Create Payment Link</button>
                 </div>
                 <div class="table-responsive m-t-40">
-                    <input type="hidden" id="totalpayment"/>
+                    <input type="hidden" id="totalpayment" />
                     <table class="table table-bordered">
                         <thead>
 
@@ -199,35 +196,35 @@
             </div>
         </div>
         <div id="OrderStatus" class="tab-pane">
-        <div class="col-sm-12">
-                    <div class="card">
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <div class="form-group">
-                                    <label class="control-label">Order Status</label>
-                                </div>
-                            </div>
-                            <div class="col-sm-3">
-                                <div class="form-group">
-                                    <label class="control-label">Order Conformation</label>
-                                </div>
-                            </div>
-                            <div class="col-sm-3">
-                                <div class="form-group">
-                                <label class="control-label">Change Expected Delivery Date </label>
-                                </div>
-                            </div>
-                            <div class="col-sm-3">
-                                <div class="form-group">
-                                <label class="control-label">Change Final Delivery Date</label>
-                                </div>
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <div class="form-group">
+                                <label class="control-label">Order Status</label>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <div class="form-group">
+                        <div class="col-sm-3">
+                            <div class="form-group">
+                                <label class="control-label">Order Conformation</label>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="form-group">
+                                <label class="control-label">Change Expected Delivery Date </label>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="form-group">
+                                <label class="control-label">Change Final Delivery Date</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <div class="form-group">
 
-                                    <select class="form-control form-control-sm"  style="width:100%" id="statusOfOrder">
+                                <select class="form-control form-control-sm" style="width:100%" id="statusOfOrder">
                                     <option value="0">Not completed</option>
                                     <option value="1">Confirmed</option>
                                     <option value="2">Processing</option>
@@ -235,39 +232,94 @@
                                     <option value="4">Completed</option>
                                     <option value="5">Cancelled</option>
                                     <option value="6">For Alteration</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-sm-3">
-                                <div class="form-group">
-                                <select class="form-control form-control-sm"  style="width:100%" id="confirmationOfOrder">
-                                <option value="0">Not Confirmed</option>
-                                <option value="1">Confirmed</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-sm-3">
-                            <input type="date" class="form-control form-control-sm" id="dateOfExpected" value="<?php echo  date('Y-m-d');?>">
-                            </div>
-                            <div class="col-sm-3">
-                            <input type="date" class="form-control form-control-sm" id="dateOfFinalDelivery" value="<?php echo  date('Y-m-d');?>">
+                                </select>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-sm-4">
+                        <div class="col-sm-3">
+                            <div class="form-group">
+                                <select class="form-control form-control-sm" style="width:100%" id="confirmationOfOrder">
+                                    <option value="0">Not Confirmed</option>
+                                    <option value="1">Confirmed</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <input type="date" class="form-control form-control-sm" id="dateOfExpected" value="<?php echo  date('Y-m-d'); ?>">
+                        </div>
+                        <div class="col-sm-3">
+                            <input type="date" class="form-control form-control-sm" id="dateOfFinalDelivery" value="<?php echo  date('Y-m-d'); ?>">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-4">
 
-                            </div>
-                            <div class="col-sm-4">
-                                <button type="button" class="btn btn-success" id="updateorderstatus">Update Status</button>
-                            </div>
-                            <div class="col-sm-4">
+                        </div>
+                        <div class="col-sm-4">
+                            <button type="button" class="btn btn-success" id="updateorderstatus">Update Status</button>
+                        </div>
+                        <div class="col-sm-4">
 
-                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+        <div id="appointments" class="tab-pane"><br>
+            <div class="card">
+
+                <div class="table-responsive m-t-40">
+
+                    <table class="table table-bordered appointTable">
+                        <thead>
+                            <tr>
+
+                                <th>Customer Name</th>
+                                <th>Appointment Date</th>
+                                <th style='display:none;'>Date</th>
+                                <th>Slot Time</th>
+                                <th>Address</th>
+                                <th>City</th>
+                                <th>Contact No</th>
+                                <th>Employee Name</th>
+                                <th>Status</th>
+                                <th style='display:none;'>Appointment ID</th>
+                                <th>Action </th>
+                            </tr>
+                        </thead>
+                        <tbody id="appointData">
+
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+            <div class="col-sm-3"></div>
+            <div class="col-6 app" style="display:none;">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Product Details</h4>
+
+                                <div class="table-responsive m-t-40">
+                                    <table id="appointdetailtbl" class="display nowrap table">
+                                        <thead>
+                                            <tr>
+                                                <!-- <th>Id</th> -->
+                                                <th>Product Title</th>
+                                                <th>Fabrics Title</th>
+
+                                            </tr>
+                                        </thead>
+                                        <tbody id="appointdetailtbldata">
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3"></div>
         </div>
     </div>
+    <?php include 'appointment_script.php';?>
     <?php include 'add_measurment.php'; ?>
     <?php include 'add_styles.php'; ?>
     <?php include 'add_fabrics.php'; ?>
